b90ba35fbe47fd6abd320e05dee1c3ac10c5d92c
NeuralFox <104081342+jyks39@users.noreply.github.com>
2025-08-20 20:20:34 +0400

    Add files via upload

---
+++ Search engine/document.cpp
@@ -0,0 +1,11 @@
+#include "document.h"
+
+    Document::Document(int id, double relevance, int rating)
+        : id(id)
+        , relevance(relevance)
+        , rating(rating) {
+    }
+
+std::ostream& operator << (std::ostream& out, const Document search){
+    return out << "{ document_id = " << search.id << ", relevance = " << search.relevance << ", rating = " << search.rating << " }";
+}
